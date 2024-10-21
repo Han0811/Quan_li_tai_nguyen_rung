fe8aae49692a50d7feacebe6b873c2d4b10303fb
Dkhang <caodanhkhang23@gmail.com>
2024-10-21 19:00:52 +0700

    update

--- Backend/forest_resource_management/src/main/java/com/project/forest_resource_management/controller/UserController.java
+++ Backend/forest_resource_management/src/main/java/com/project/forest_resource_management/controller/UserController.java
@@ -1,12 +1,10 @@
 package com.project.forest_resource_management.controller;
 
-import ch.qos.logback.core.model.Model;
 import com.project.forest_resource_management.dtos.LoginDTO;
 import com.project.forest_resource_management.dtos.UserDTO;
 import com.project.forest_resource_management.models.UserEntity;
 import com.project.forest_resource_management.servicies.impl.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -35,9 +33,9 @@ public class UserController {
     }
 
     @GetMapping("/list")
-    public  String listUser(Model model) {
+    public  String listUser() {
         List<UserEntity> users = userService.findAllUsers();
-        model.addText("");
+//        model.addText("");
         return "";
     }
 
@@ -47,21 +45,25 @@ public class UserController {
     }
 
     @GetMapping("/login")
-    public String showLoginForm(Model model) {
-        model.addText("loginDTO");
+    public String showLoginForm(org.springframework.ui.Model model) {
+        model.addAttribute("loginDTO", new LoginDTO());
         return "login"; // Trả về view login.html
     }
 
     @PostMapping("/login")
-    public String login(@ModelAttribute LoginDTO loginDTO, Model model) {
+    public String login(@ModelAttribute LoginDTO loginDTO) {
         // Kiểm tra thông tin đăng nhập (logic của bạn)
         boolean isAuthenticated = true; // Chỉ là ví dụ
 
         if (isAuthenticated) {
-            return "redirect:/home"; // Chuyển hướng đến trang chính khi đăng nhập thành công
+            return "redirect:/api/user/main_screen"; // Chuyển hướng đến trang chính khi đăng nhập thành công
         } else {
-            model.addText("errorMessage");
             return "login"; // Trả về trang đăng nhập nếu thất bại
         }
+    }
+
+    @GetMapping("/main_screen")
+    public String showMainScreen() {
+        return "main_screen"; // Trả về view main_screen.html
     }
 }
